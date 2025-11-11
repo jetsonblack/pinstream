40f8a597f053d8a792b1c29c86a02b135405df83
Jetson Black <126121616+jetsonblack@users.noreply.github.com>
2025-11-11 13:06:49 -0500

    Update shell.js

--- scripts/shell.js
+++ scripts/shell.js
@@ -5,10 +5,10 @@ dotenv.config({ path: __dirname + '/../.env' });
 mongoose.set('strictQuery', true);
 
 const Pin = require('../server/models/Pin');
-const User = require('../server/models/User'); // 👈 this line fixes the error
+const User = require('../server/models/User');
 
 async function run() {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URL);
   const result = await Pin.deleteMany({});
   console.log(`✅ Deleted ${result.deletedCount} pins.`);
   process.exit();
